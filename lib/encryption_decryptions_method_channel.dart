524c0bee6631fa78c7c9d88deeb70a0d9042fcd1
dev-devarsh <180130111055@gecg28.ac.in>
2023-05-21 22:07:11 +0530

    chnage base methods

--- lib/encryption_decryptions_method_channel.dart
+++ lib/encryption_decryptions_method_channel.dart
@@ -4,17 +4,17 @@ import 'package:flutter/services.dart';
 import 'encryption_decryptions_platform_interface.dart';
 
 /// An implementation of [EncryptionDecryptionsPlatform] that uses method channels.
-class MethodChannelEncryptionDecryptions extends EncryptionDecryptionsPlatform {
+class MethodChannelEncryptionDecryptions implements EncryptionDecryptionsPlatform {
   /// The method channel used to interact with the native platform.
   @visibleForTesting
   final methodChannel = const MethodChannel('encryption_decryptions');
 
   @override
-  Future<String?> getEncryptedData({required String data, required String key}) {
+  Future<String?> encryptData({required String data, required String key}) {
     return EncryptionDecryptionsPlatform.instance.encryptData(data: data, key: key);
   }
   @override
-  Future<String?> getDecryptedData({required String data, required String key}) {
+  Future<String?> decryptData({required String data, required String key}) {
     return EncryptionDecryptionsPlatform.instance.decryptData(data: data, key: key);
   }
 }
