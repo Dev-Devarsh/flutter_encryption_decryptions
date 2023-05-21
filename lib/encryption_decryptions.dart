0e466ba93e1128ff3a1d04a8887ee8a88cc84084
Devarsh Panchal <180130111055@gecg28.ac.in>
2023-05-21 22:21:53 +0530

    Update encryption_decryptions.dart
    make method static

--- lib/encryption_decryptions.dart
+++ lib/encryption_decryptions.dart
@@ -1,10 +1,10 @@
 import 'encryption_decryptions_platform_interface.dart';
 
 class EncryptionDecryptions {
-  Future<String?> getEncryptedData({required String data, required String key}) {
+ static Future<String?> getEncryptedData({required String data, required String key}) {
     return EncryptionDecryptionsPlatform.instance.encryptData(data: data, key: key);
   }
-  Future<String?> getDecryptedData({required String data, required String key}) {
+static  Future<String?> getDecryptedData({required String data, required String key}) {
     return EncryptionDecryptionsPlatform.instance.decryptData(data: data, key: key);
   }
 }
