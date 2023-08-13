82b4bde8dccfc78f8734c312b109120d4c67fde2
dev-devarsh <180130111055@gecg28.ac.in>
2023-08-13 11:49:30 +0530

    fix: android null values

--- android/src/main/kotlin/com/encryption/encryption_decryptions/EncryptionDecryptionsPlugin.kt
+++ android/src/main/kotlin/com/encryption/encryption_decryptions/EncryptionDecryptionsPlugin.kt
@@ -1,12 +1,16 @@
 package com.encryption.encryption_decryptions
 
 import androidx.annotation.NonNull
-
-import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import android.provider.Settings
+import android.content.Context
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import tgio.rncryptor.RNCryptorNative
+
 
 /** EncryptionDecryptionsPlugin */
 class EncryptionDecryptionsPlugin: FlutterPlugin, MethodCallHandler {
